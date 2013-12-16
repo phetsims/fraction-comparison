af60d428bee408fbe61017a3319e5d6bb090c457
samreid <reids@colorado.edu>
2013-12-16 13:25:55 -0700

    initial check-in with phet framework scaffolding

---
+++ js/intro/model/IntroModel.js
@@ -0,0 +1,18 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * Model for the 'Intro' screen.
+ *
+ * @author Sam Reid
+ */
+define( function( require ) {
+  'use strict';
+
+  function IntroModel() {
+  }
+
+  IntroModel.prototype = {
+    step: function( dt ) {}
+  };
+  return IntroModel;
+} );
---
+++ js/intro/view/IntroView.js
@@ -0,0 +1,27 @@
+// Copyright 2002-2013, University of Colorado Boulder
+
+/**
+ * View for the 'Intro' screen.
+ *
+ * @author Sam Reid
+ */
+define( function( require ) {
+  'use strict';
+
+  // imports
+  var Bounds2 = require( 'DOT/Bounds2' );
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var inherit = require( 'PHET_CORE/inherit' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
+  var ScreenView = require( 'JOIST/ScreenView' );
+
+  function IntroView( model ) {
+
+    var thisView = this;
+    ScreenView.call( thisView, { renderer: 'svg' } );
+  }
+
+  return inherit( ScreenView, IntroView );
+} );
