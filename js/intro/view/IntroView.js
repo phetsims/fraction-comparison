3a48dd96db848677a081840db428568f207b4927
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-02-25 11:23:17 -0700

    Remove "and" method from Property API, see https://github.com/phetsims/axon/issues/39

--- js/intro/view/IntroView.js
+++ js/intro/view/IntroView.js
@@ -154,15 +154,15 @@ define( function( require ) {
 
     var leftDivisionsProperty = model.leftFractionModel.property( 'divisions' );
     var leftDivisionSpinner = new LeftRightSpinner( leftDivisionsProperty,
-      leftDivisionsProperty.greaterThanNumber( 1 ),
-      leftDivisionsProperty.lessThanNumber( 10 ),
+      new DerivedProperty( [leftDivisionsProperty], function( leftDivisions ) { return leftDivisions > 1; } ),
+      new DerivedProperty( [leftDivisionsProperty], function( leftDivisions ) { return leftDivisions < 10; } ),
       { centerX: leftHorizontalBarContainerNode.centerX, top: leftHorizontalBarContainerNode.bottom + 6 } );
     this.addChild( leftDivisionSpinner );
 
     var rightDivisionsProperty = model.rightFractionModel.property( 'divisions' );
     var rightDivisionSpinner = new LeftRightSpinner( rightDivisionsProperty,
-      rightDivisionsProperty.greaterThanNumber( 1 ),
-      rightDivisionsProperty.lessThanNumber( 10 ),
+      new DerivedProperty( [rightDivisionsProperty], function( rightDivisions ) { return rightDivisions > 1; } ),
+      new DerivedProperty( [rightDivisionsProperty], function( rightDivisions ) { return rightDivisions < 10; } ),
       { centerX: rightHorizontalBarContainerNode.centerX, top: rightHorizontalBarContainerNode.bottom + 6 } );
     this.addChild( rightDivisionSpinner );
 
