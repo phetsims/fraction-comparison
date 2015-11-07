32bc8f9851d45a3587d91e067c66ec66a514c81a
samreid <reids@colorado.edu>
2015-11-06 20:50:36 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/intro/view/NodeDragHandler.js
+++ js/intro/view/NodeDragHandler.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2013, University of Colorado Boulder
+// Copyright 2013, University of Colorado Boulder
 
 /**
  * A drag handler for something that is movable and constrained to some (optional) bounds.
