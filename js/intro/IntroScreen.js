88ba83cc02b0079a9a4d1bba56f0b98855fa3c9a
Chris Malley <cmalley@pixelzoom.com>
2016-09-08 15:49:11 -0600

    fix size of default home screen icons, https://github.com/phetsims/joist/issues/352

--- js/intro/IntroScreen.js
+++ js/intro/IntroScreen.js
@@ -15,14 +15,14 @@ define( function( require ) {
   var Screen = require( 'JOIST/Screen' );
   var IntroModel = require( 'FRACTION_COMPARISON/intro/model/IntroModel' );
   var IntroView = require( 'FRACTION_COMPARISON/intro/view/IntroView' );
-  var Text = require( 'SCENERY/nodes/Text' );
+  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
 
   var screenTitle = 'Intro';
 
   function IntroScreen() {
     Screen.call( this,
       screenTitle,
-      new Text( 'hello' ),
+      new Rectangle( 0, 0, Screen.HOME_SCREEN_ICON_SIZE.width, Screen.HOME_SCREEN_ICON_SIZE.height, { fill: 'white' } ),
       function() { return new IntroModel(); },
       function( model ) { return new IntroView( model, ModelViewTransform2.createIdentity() ); },
       { backgroundColor: '#e1f1f1' }
