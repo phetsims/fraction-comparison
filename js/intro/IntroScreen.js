09b68311a4ccd6aa59f479e6f8d624d183adb885
Chris Malley <cmalley@pixelzoom.com>
2016-09-08 23:26:23 -0600

    change clients to use new Screen constructor signature, https://github.com/phetsims/joist/issues/339, https://github.com/phetsims/joist/issues/351

--- js/intro/IntroScreen.js
+++ js/intro/IntroScreen.js
@@ -15,14 +15,9 @@ define( function( require ) {
   var Screen = require( 'JOIST/Screen' );
   var IntroModel = require( 'FRACTION_COMPARISON/intro/model/IntroModel' );
   var IntroView = require( 'FRACTION_COMPARISON/intro/view/IntroView' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
-
-  var screenTitle = 'Intro';
 
   function IntroScreen() {
     Screen.call( this,
-      screenTitle,
-      new Rectangle( 0, 0, Screen.HOME_SCREEN_ICON_SIZE.width, Screen.HOME_SCREEN_ICON_SIZE.height, { fill: 'white' } ),
       function() { return new IntroModel(); },
       function( model ) { return new IntroView( model, ModelViewTransform2.createIdentity() ); },
       { backgroundColor: '#e1f1f1' }
