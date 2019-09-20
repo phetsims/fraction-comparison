6ad3403e80d833c985d5fc19baa72e9b014c9cab
phet-dev <phetadmin@gmail.com>
2019-09-20 03:13:02 -0600

    update copyright dates from daily grunt work

--- js/intro/view/NodeDragHandler.js
+++ js/intro/view/NodeDragHandler.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2019, University of Colorado Boulder
 
 /**
  * A drag handler for something that is movable and constrained to some (optional) bounds.
