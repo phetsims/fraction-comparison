25bada4bc8383a5b81c8cb31e80b4e71ef3db85b
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:35:19 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/intro/IntroScreen.js
+++ js/intro/IntroScreen.js
@@ -8,21 +8,20 @@
 
 import Property from '../../../axon/js/Property.js';
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import ModelViewTransform2 from '../../../phetcommon/js/view/ModelViewTransform2.js';
 import fractionComparison from '../fractionComparison.js';
 import IntroModel from './model/IntroModel.js';
 import IntroView from './view/IntroView.js';
 
-function IntroScreen() {
-  Screen.call( this,
-    function() { return new IntroModel(); },
-    function( model ) { return new IntroView( model, ModelViewTransform2.createIdentity() ); },
-    { backgroundColorProperty: new Property( '#e1f1f1' ) }
-  );
+class IntroScreen extends Screen {
+  constructor() {
+    super(
+      function() { return new IntroModel(); },
+      function( model ) { return new IntroView( model, ModelViewTransform2.createIdentity() ); },
+      { backgroundColorProperty: new Property( '#e1f1f1' ) }
+    );
+  }
 }
 
 fractionComparison.register( 'IntroScreen', IntroScreen );
-
-inherit( Screen, IntroScreen );
 export default IntroScreen;
