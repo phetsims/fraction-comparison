e2ebad756e9448303b99e286b9487761bde4a01e
pixelzoom <cmalley@pixelzoom.com>
2020-10-01 21:41:15 -0600

    convert Panel subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/intro/view/RepresentationPanel.js
+++ js/intro/view/RepresentationPanel.js
@@ -6,7 +6,6 @@
  * @author Sam Reid
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
@@ -18,55 +17,54 @@ import fractionComparison from '../../fractionComparison.js';
 // constants
 const ICON_SCALE = 0.75;
 
-/**
- *
- * @param {Property.<string>} representationProperty
- * @param {Object} [options]
- * @constructor
- */
-function RepresentationPanel( representationProperty, options ) {
+class RepresentationPanel extends Panel {
 
-  options = merge( {
-    fill: '#efe8e1',
-    xMargin: 10,
-    yMargin: 7
-  }, options );
+  /**
+   * @param {Property.<string>} representationProperty
+   * @param {Object} [options]
+   */
+  constructor( representationProperty, options ) {
 
-  const content = new RadioButtonGroup( representationProperty, [
-    {
-      value: 'horizontal-bar',
-      node: new Rectangle( 0, 0, 50 * ICON_SCALE, 30 * ICON_SCALE, { fill: '#208644', lineWidth: 1, stroke: 'black' } )
-    },
-    {
-      value: 'vertical-bar',
-      node: new Rectangle( 0, 0, 14 * ICON_SCALE, 50 * ICON_SCALE, { fill: 'red', lineWidth: 1, stroke: 'black' } )
-    },
-    { value: 'circle', node: new Circle( 22 * ICON_SCALE, { fill: '#145991', lineWidth: 1, stroke: 'black' } ) },
-    {
-      value: 'chocolate',
-      node: new Rectangle( 0, 0, 50 * ICON_SCALE, 40 * ICON_SCALE, { fill: '#563329', lineWidth: 1, stroke: 'black' } )
-    },
-    {
-      value: 'different-sized-circles',
-      node: new Node( {
-        children: [
-          new Circle( 20 * ICON_SCALE, { fill: '#f0d041', lineWidth: 1, stroke: 'black', x: 26, y: -26 } ),
-          new Circle( 12 * ICON_SCALE, { fill: '#f0d041', lineWidth: 1, stroke: 'black' } )
-        ]
-      } )
-    }
-  ], {
-    // RadioButtonGroup options
-    orientation: 'horizontal',
-    baseColor: 'white',
-    cornerRadius: 10,
-    spacing: 12
-  } );
+    options = merge( {
+      fill: '#efe8e1',
+      xMargin: 10,
+      yMargin: 7
+    }, options );
 
-  Panel.call( this, content, options );
+    const content = new RadioButtonGroup( representationProperty, [
+      {
+        value: 'horizontal-bar',
+        node: new Rectangle( 0, 0, 50 * ICON_SCALE, 30 * ICON_SCALE, { fill: '#208644', lineWidth: 1, stroke: 'black' } )
+      },
+      {
+        value: 'vertical-bar',
+        node: new Rectangle( 0, 0, 14 * ICON_SCALE, 50 * ICON_SCALE, { fill: 'red', lineWidth: 1, stroke: 'black' } )
+      },
+      { value: 'circle', node: new Circle( 22 * ICON_SCALE, { fill: '#145991', lineWidth: 1, stroke: 'black' } ) },
+      {
+        value: 'chocolate',
+        node: new Rectangle( 0, 0, 50 * ICON_SCALE, 40 * ICON_SCALE, { fill: '#563329', lineWidth: 1, stroke: 'black' } )
+      },
+      {
+        value: 'different-sized-circles',
+        node: new Node( {
+          children: [
+            new Circle( 20 * ICON_SCALE, { fill: '#f0d041', lineWidth: 1, stroke: 'black', x: 26, y: -26 } ),
+            new Circle( 12 * ICON_SCALE, { fill: '#f0d041', lineWidth: 1, stroke: 'black' } )
+          ]
+        } )
+      }
+    ], {
+      // RadioButtonGroup options
+      orientation: 'horizontal',
+      baseColor: 'white',
+      cornerRadius: 10,
+      spacing: 12
+    } );
+
+    super( content, options );
+  }
 }
 
 fractionComparison.register( 'RepresentationPanel', RepresentationPanel );
-
-inherit( Panel, RepresentationPanel );
 export default RepresentationPanel;
